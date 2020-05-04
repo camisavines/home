f0eb23fbcf6bc3df018d1f4733cddd4c9c2788af
cdvines@iu.edu <cdvines623@gmail.com>
2020-05-04 17:41:06 -0400

    building static site

---
+++ build/precache-manifest.b186a408ed13281ab5b1926d63118576.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af418a799ac66c106e3148f08220ebc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc7479621477c400a089",
+    "url": "/static/css/main.e81a2ebc.chunk.css"
+  },
+  {
+    "revision": "a2bcd8a7db7e4d923592",
+    "url": "/static/js/2.de06a388.chunk.js"
+  },
+  {
+    "revision": "890b83c3b503af9a5630afd9ff112890",
+    "url": "/static/js/2.de06a388.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc7479621477c400a089",
+    "url": "/static/js/main.14bf4167.chunk.js"
+  },
+  {
+    "revision": "7e65d857e43f4173d1e8",
+    "url": "/static/js/runtime-main.7813ffa7.js"
+  },
+  {
+    "revision": "2e9f8d3f5d37b39216f5de6a5fc218c8",
+    "url": "/static/media/bgc.2e9f8d3f.png"
+  },
+  {
+    "revision": "aeb524c2c5c63ec8ef2d8606be8b4c5a",
+    "url": "/static/media/givegrove.aeb524c2.png"
+  },
+  {
+    "revision": "8ee9a5e1363311c41065d51280805e4a",
+    "url": "/static/media/iu.8ee9a5e1.jpg"
+  },
+  {
+    "revision": "f38f9cf1608645fde799132c9b3a873f",
+    "url": "/static/media/karyosoft.f38f9cf1.png"
+  },
+  {
+    "revision": "af23a270f743899c5fe6a35b0496d764",
+    "url": "/static/media/luma.af23a270.png"
+  },
+  {
+    "revision": "8565c3d880671d5823f790f154a3489a",
+    "url": "/static/media/sbcs_logo.8565c3d8.png"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.84a80757a614fdcf2ae3355aba8ed07d.js"
+  "/precache-manifest.b186a408ed13281ab5b1926d63118576.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
