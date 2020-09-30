b98971d8bf4e32e734acd0469e66829e79e6fb77
cdvines <cdvines@iu.edu>
2020-09-30 02:54:37 -0400

    update home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,9 +5,10 @@ function Home() {
     return (
         <div>
             <h1 style={{fontSize: "4em", color: "black"}}>Camisa Vines</h1>
-            <p className="bigger-p">Computer Science student at Indiana University-Bloomington pursuing minors in Business, Math, and Sport Analytics.</p>
-            <p className="bigger-p">A highly capable, motivated, and innovative young professional with an exemplary academic report, and a passion for continuous learning and development, and interest in digtial sports analytics. Has the ability to work well, both independently and as part of a team. Always eager for a challenge and willing to address unfamiliar tasks.</p>
-            <p className="bigger-p">Although only a second-year, undergraduate student, has 4 years of professional experiences as a frontend web developer and software engineer. Is proficienct in many programming languages and applications. With considerable technical ability, other skills include teamwork, leadership, organization, written & verbal communication, and research & documentation.</p>
+            <p className="bigger-p">Computer Science student at Indiana University-Bloomington <br/> Specializing in Software Engineering with minors in Business and Math.</p>
+            <p className="bigger-p">Jobs that interest me are Technology Consultant, Data Analyst, and Software Engineer.</p>
+            <p className="bigger-p">I am a highly capable and innovative young professional with an exemplary academic report, and a passion for continuous learning and development. I am have the ability to work well both independently and part of a team. I am always willing to address challenging and unfamiliar tasks.
+                                    Although I am only a third year, undergraduate student, I have internship experience as a frontend web developer and software engineer. I am proficienct in many programming languages and platforms and am confident in my ability to succeed in roles similar to these.</p>
         </div>
     )
 }
