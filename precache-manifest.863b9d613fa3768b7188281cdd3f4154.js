b715d49ea125287a11e464206120f857323346d3
cdvines <cdvines@iu.edu>
2020-10-08 02:38:20 -0400

    Updates

--- precache-manifest.863b9d613fa3768b7188281cdd3f4154.js
+++ precache-manifest.863b9d613fa3768b7188281cdd3f4154.js
@@ -1,27 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cff9e87f55f9c862c9b2195e5a89f79d",
+    "revision": "86142c6bcd08f3e79371f6358dae7177",
     "url": "/home/index.html"
   },
   {
-    "revision": "840a2375b92db7f782ac",
+    "revision": "c7647616614bf27b2161",
     "url": "/home/static/css/main.c8ae8804.chunk.css"
   },
   {
-    "revision": "66d5ca64df53a87f56c0",
-    "url": "/home/static/js/2.61431cb1.chunk.js"
+    "revision": "98060d1bcd2844a4c1fb",
+    "url": "/home/static/js/2.f4349b12.chunk.js"
   },
   {
-    "revision": "890b83c3b503af9a5630afd9ff112890",
-    "url": "/home/static/js/2.61431cb1.chunk.js.LICENSE.txt"
+    "revision": "b1c48fb50b52ff7e51ea57a4c76b798d",
+    "url": "/home/static/js/2.f4349b12.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "840a2375b92db7f782ac",
-    "url": "/home/static/js/main.722e45bf.chunk.js"
+    "revision": "c7647616614bf27b2161",
+    "url": "/home/static/js/main.03e8ab94.chunk.js"
   },
   {
     "revision": "fc580907e01d6f161f68",
     "url": "/home/static/js/runtime-main.4ee144fb.js"
+  },
+  {
+    "revision": "945b77c7609c9296bcdef07e51b6fdf9",
+    "url": "/home/static/media/bdb-pro.945b77c7.png"
   },
   {
     "revision": "2e9f8d3f5d37b39216f5de6a5fc218c8",
@@ -32,12 +36,20 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/home/static/media/camisa.0f007dca.jpg"
   },
   {
+    "revision": "b808996ba691c0d8c268a995f28afd83",
+    "url": "/home/static/media/fw-pro.b808996b.png"
+  },
+  {
     "revision": "aeb524c2c5c63ec8ef2d8606be8b4c5a",
     "url": "/home/static/media/givegrove.aeb524c2.png"
   },
   {
     "revision": "8ee9a5e1363311c41065d51280805e4a",
     "url": "/home/static/media/iu.8ee9a5e1.jpg"
+  },
+  {
+    "revision": "496fb429ce45e2d3b47a5d74af799938",
+    "url": "/home/static/media/jr-pro.496fb429.png"
   },
   {
     "revision": "f38f9cf1608645fde799132c9b3a873f",
@@ -64,7 +76,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/home/static/media/news4.978705b0.png"
   },
   {
+    "revision": "81e034ce28df5d2c287fd675c7a4592d",
+    "url": "/home/static/media/regrader-pro.81e034ce.png"
+  },
+  {
     "revision": "8565c3d880671d5823f790f154a3489a",
     "url": "/home/static/media/sbcs_logo.8565c3d8.png"
+  },
+  {
+    "revision": "6facae51922b9bc4ae1ddcd18f7a3e1c",
+    "url": "/home/static/media/time-pro.6facae51.png"
   }
 ]);
