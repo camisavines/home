742edc7fafffae0d4bb48ab1ac3ab9d48ad00b30
cdvines <cdvines@iu.edu>
2021-08-20 14:36:24 -0400

    Updates

---
+++ precache-manifest.418643e7dba7555f5d5410b65bec0709.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "349d913cb1c2841dddcd4eaa6f1c19d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "887adfcf790c5e8dd593",
+    "url": "/static/css/main.1f1bfe74.chunk.css"
+  },
+  {
+    "revision": "6cef3e1059a152115e08",
+    "url": "/static/js/2.fea2c61a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.fea2c61a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "887adfcf790c5e8dd593",
+    "url": "/static/js/main.66ba4bfc.chunk.js"
+  },
+  {
+    "revision": "771ae747d194584fe154",
+    "url": "/static/js/runtime-main.66ff130e.js"
+  },
+  {
+    "revision": "663e6a6f9e4c578800544d23368ed0f3",
+    "url": "/static/media/00-Logo.663e6a6f.png"
+  },
+  {
+    "revision": "7a93ab7781ce366be3a379fd86567a26",
+    "url": "/static/media/01-Logo.7a93ab77.png"
+  },
+  {
+    "revision": "5a10599009935a70bcd21a81e60415ff",
+    "url": "/static/media/blackdatabase.5a105990.png"
+  },
+  {
+    "revision": "0527ee4431f4e550883e2c331974ccfe",
+    "url": "/static/media/covidfacts.0527ee44.png"
+  },
+  {
+    "revision": "f8f4ff92214374cc2b385c8fac363430",
+    "url": "/static/media/dogs.f8f4ff92.JPG"
+  },
+  {
+    "revision": "8adf2aaa8626b4fb4466b3567c148463",
+    "url": "/static/media/futurewonder.8adf2aaa.png"
+  },
+  {
+    "revision": "0938cc05ed3420f1c809973676eb6d6f",
+    "url": "/static/media/insta.0938cc05.PNG"
+  },
+  {
+    "revision": "96e9e31bfd8784204a9c898edbcfb168",
+    "url": "/static/media/insta3.96e9e31b.jpg"
+  },
+  {
+    "revision": "74aa54de03e2c3e1e3a90cab8703dd4a",
+    "url": "/static/media/kualoa.74aa54de.JPG"
+  },
+  {
+    "revision": "0d70ce161f310f6077709e58e65603aa",
+    "url": "/static/media/me.0d70ce16.JPG"
+  },
+  {
+    "revision": "a3ff63a05bad921ffb0caea79d54b97b",
+    "url": "/static/media/news1.a3ff63a0.jpeg"
+  },
+  {
+    "revision": "93d8982d396206662067e9bed4d97591",
+    "url": "/static/media/news2.93d8982d.jpg"
+  },
+  {
+    "revision": "5a57e4b548761d3bc7f1dbb9286b0494",
+    "url": "/static/media/news3.5a57e4b5.jpg"
+  },
+  {
+    "revision": "978705b05acfc0d878a125df6ae8829d",
+    "url": "/static/media/news4.978705b0.png"
+  },
+  {
+    "revision": "9a0560a13c27e2c970c481458882a91f",
+    "url": "/static/media/news5.9a0560a1.webp"
+  },
+  {
+    "revision": "3a19f6430cef4e36fb9cdb37dc731eab",
+    "url": "/static/media/news6.3a19f643.png"
+  },
+  {
+    "revision": "bb767d371dbe3042f4996ed9359f0ba6",
+    "url": "/static/media/python_project.bb767d37.png"
+  },
+  {
+    "revision": "4d84db4a69fe35604d3b374da2f6ce9a",
+    "url": "/static/media/sunset.4d84db4a.JPG"
+  },
+  {
+    "revision": "fbd68b15e1ec53156f4c3c3466153947",
+    "url": "/static/media/train.fbd68b15.jpg"
+  },
+  {
+    "revision": "ef6e8de54c65b8fed0e888fe8f6a3aab",
+    "url": "/static/media/turtle.ef6e8de5.jpg"
+  }
+]);
