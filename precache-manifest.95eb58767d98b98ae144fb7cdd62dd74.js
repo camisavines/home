89a9ff2147176cb6ea6b869912d9ac9e26818231
cdvines <cdvines@iu.edu>
2021-09-27 14:49:51 -0400

    Updates

---
+++ precache-manifest.95eb58767d98b98ae144fb7cdd62dd74.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ac0d9d2dc87621a7206b2c8ede9d27e",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "d53b091dd8295116f463",
+    "url": "/home/static/css/main.fb29bfd1.chunk.css"
+  },
+  {
+    "revision": "8af625a72dfb70e5941f",
+    "url": "/home/static/js/2.2eb103bc.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/home/static/js/2.2eb103bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d53b091dd8295116f463",
+    "url": "/home/static/js/main.4f5d9bdf.chunk.js"
+  },
+  {
+    "revision": "776b0b3b8ceeacd53e3e",
+    "url": "/home/static/js/runtime-main.b3cf67a7.js"
+  },
+  {
+    "revision": "5a10599009935a70bcd21a81e60415ff",
+    "url": "/home/static/media/blackdatabase.5a105990.png"
+  },
+  {
+    "revision": "0527ee4431f4e550883e2c331974ccfe",
+    "url": "/home/static/media/covidfacts.0527ee44.png"
+  },
+  {
+    "revision": "f8f4ff92214374cc2b385c8fac363430",
+    "url": "/home/static/media/dogs.f8f4ff92.JPG"
+  },
+  {
+    "revision": "8adf2aaa8626b4fb4466b3567c148463",
+    "url": "/home/static/media/futurewonder.8adf2aaa.png"
+  },
+  {
+    "revision": "0938cc05ed3420f1c809973676eb6d6f",
+    "url": "/home/static/media/insta.0938cc05.PNG"
+  },
+  {
+    "revision": "96e9e31bfd8784204a9c898edbcfb168",
+    "url": "/home/static/media/insta3.96e9e31b.jpg"
+  },
+  {
+    "revision": "74aa54de03e2c3e1e3a90cab8703dd4a",
+    "url": "/home/static/media/kualoa.74aa54de.JPG"
+  },
+  {
+    "revision": "0d70ce161f310f6077709e58e65603aa",
+    "url": "/home/static/media/me.0d70ce16.JPG"
+  },
+  {
+    "revision": "bb767d371dbe3042f4996ed9359f0ba6",
+    "url": "/home/static/media/python_project.bb767d37.png"
+  },
+  {
+    "revision": "4d84db4a69fe35604d3b374da2f6ce9a",
+    "url": "/home/static/media/sunset.4d84db4a.JPG"
+  },
+  {
+    "revision": "fbd68b15e1ec53156f4c3c3466153947",
+    "url": "/home/static/media/train.fbd68b15.jpg"
+  },
+  {
+    "revision": "ef6e8de54c65b8fed0e888fe8f6a3aab",
+    "url": "/home/static/media/turtle.ef6e8de5.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1cd1ec11400655e315788827527cb48e.js"
+  "/home/precache-manifest.95eb58767d98b98ae144fb7cdd62dd74.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
