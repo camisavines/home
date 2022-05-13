f6a774bcaca9708afb904024d5ef304c023db571
camisavines <camisa.vines@icloud.com>
2022-05-13 18:27:20 -0400

    deploy

--- src/pages/news.js
+++ src/pages/news.js
@@ -3,27 +3,6 @@ import '../css/news.css';
 
 import posts from '../data/news_content';
 
-import timeline from '../data/timeline';
-
-const NEWS = posts.map(p =>
-    <div className="my-3">
-        <div className="card">
-            {p.image ? (
-                <img className="card-img-top" src={p.image} alt={p.title} style={{border: "10px solid white"}}/>
-            ) : (<></>)}
-            <div className="card-body">
-                <h3>{p.title}</h3>
-                <p className="card-text">{p.description}</p>
-                <p><small className="text-muted m-0">{p.date} | <a href={p.link}>Visit</a></small></p>
-            </div>
-        </div>
-    </div>
-    
-)
-
-const column1 = NEWS.slice(0, posts.length/2);
-const column2 = NEWS.slice(posts.length/2, posts.length);
-
 function News() {
     return (
         <>
