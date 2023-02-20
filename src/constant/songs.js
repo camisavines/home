12cbc3cb0c1931eef6d4e6758d042ed3a46a61bd
camisavines <camisa.vines@icloud.com>
2023-02-19 23:39:52 -0600

    home page updated

---
+++ src/constant/songs.js
@@ -0,0 +1,14 @@
+export const songs = [
+  {
+    title: "Forgiveness",
+    artist: "SZA",
+    album: "SOS",
+    art: "http://www.iheartradio.ca/image/policy:1.18873719:1669982885/SZAalbum.jpg?a=16%3A9&w=1000&$p$a$w=39f1bac",
+  },
+  {
+    title: "Damage",
+    artist: "H.E.R",
+    album: "Back of my Mind",
+    art: "https://media.pitchfork.com/photos/60c381b142b81a6a84121c6f/1:1/w_600/HER-Back-of-My-Mind.jpeg",
+  },
+];
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,89 @@
+import React from "react";
+import camisa from "../photos/IMG_5279.jpg";
+import { songs } from "../constant/songs";
+
 const Home = () => {
   return (
-    <h1>Welcome</h1>
-  )
-}
+    <div>
+      {/* <h1>Welcome</h1> */}
 
-export { Home }
+      <div className="row" style={{ marginTop: "2rem" }}>
+        <div className="col s12 m6 l4">
+          <div class="card" style={{ borderRadius: "10px" }}>
+            <div class="card-image">
+              <img src={camisa} />
+              {/* <span class="card-title">Camisa</span> */}
+            </div>
+            {/* <div class="card-content">
+              <p>Hey what's up?! Welcome to my site!</p>
+            </div> */}
+          </div>
+        </div>
+
+        <div className="col s12 m6 l8">
+          <div
+            style={{
+              display: "flex",
+              alignItems: "start",
+              justifyContent: "space-between",
+              flexDirection: "column",
+              minHeight: "45vh",
+            }}
+          >
+            <h3>Welcome!</h3>
+            <p>
+              Hey, there! I'm Camisa. <br /> I'm a software developer who really
+              loves music. <br /> Click the links on the left to explore and
+              checkout my top 10 songs below.
+            </p>
+
+            <div>
+              <p>Connect with me and let me know what you're listening to!</p>
+              {/* <a class="waves-effect waves-light btn red"><i class="material-icons left">play_arrow</i>Play</a> */}
+              <a class="waves-effect waves-light btn red">
+                <i class="material-icons left">add</i>Connect
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="row">
+        <table className="highlight">
+          <thead>
+            <tr>
+              <th>Song</th>
+              <th>Artist</th>
+              <th>Album</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {songs.map((song, index) => (
+              <tr>
+                <td style={{ display: "flex", alignItems: "center" }}>
+                  <p style={{ marginRight: "1rem", color: "#a1a1a1"}}>{index + 1}</p>
+                  <div
+                    style={{
+                      backgroundImage: `url(${song.art})`,
+                      height: "4rem",
+                      width: "4rem",
+                      backgroundSize: "cover",
+                      backgroundPosition: "center",
+                      marginRight: "1rem"
+                    }}
+                  />
+                  {song.title}
+                </td>
+                <td>{song.artist}</td>
+                <td>{song.album}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export { Home };
