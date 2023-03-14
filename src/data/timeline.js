8a38ded57c9d8122b4cc302c5fa25b9b9c8121f4
camisavines <camisa.vines@icloud.com>
2023-03-14 18:11:20 -0500

    update text

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,5 +1,6 @@
 import sbcs from "../photos/timeline/Pic4.jpeg";
 import ibmgif from "../photos/timeline/ibm.gif";
+import luma from "../photos/timeline/luma.png";
 
 const timeline = [
     {
@@ -17,14 +18,15 @@ const timeline = [
         bar_color: "b-primary",
         date: "August 2016",
         title: "First Internship",
-        content: "My first internship was as a web development intern building onlince courseware for universities around the US. Who knew that virtual school materials would be so crucial today?!",
+        image: luma,
+        content: "My first internship was with Luma Brighter Learning, as a web developer, building online courseware for universities around the US. Who knew that virtual school materials would be so crucial today?!",
         subtexts: ["Luma", "Notre Dame, IN"]
     },
     {
         bar_color: "b-primary",
         date: "June 2018",
-        title: "Diploma Status",
-        content: "After high school, I decided to stay continue my education and career in tech at Indiana University-Bloomington studying Computer Science.",
+        title: "Senior Year",
+        content: "After high school, I decided to continue my education and career in tech at Indiana University-Bloomington studying Computer Science.",
         subtexts: ["South Bend, IN"],
         image: "https://assets.simpleviewinc.com/simpleview/image/upload/c_fill,g_xy_center,h_573,q_60,w_573,x_1728,y_1417/v1/clients/bloomington/SampleGatesFall_Sunrise_10_edit_2f20bd0e-3463-416b-98ad-f45f347e29d1.jpg"
     },
@@ -33,7 +35,7 @@ const timeline = [
         bar_color: "b-primary",
         date: "May 2019",
         title: "Future Wonder",
-        content: "Began working at Future Wonder CO. as a Tech Consulting Intern, and then eventually as a Post-Intern",
+        content: "Began working at Future Wonder CO. as a Tech Consulting Intern, and then eventually as a Post-Intern throughout college",
         subtexts: ["Future Wonder Co.", "Bloomington, IN"],
         linkText: "futurewonder.com",
         link: "https://www.futurewonder.com/"
@@ -45,7 +47,8 @@ const timeline = [
         title: "IBM",
         content: "Began working at IBM as a Frontend Development Intern. I worked remotely in the CIO domain out of the Austin, TX office.",
         subtexts: ["IBM", "Austin, TX (remote)", "worked from Honolulu, HI"],
-        image: "https://www.consultancy.uk/media/IBM-Studio-London-14866.jpg"
+        image: "http://antunovich.com//upload/projects/207/764141798_ibm004_medium.jpg"
+        // image: "https://www.consultancy.uk/media/IBM-Studio-London-14866.jpg"
     },
     {
         item_classes: "active",
@@ -78,6 +81,6 @@ const timeline = [
 
 // <p></p>
 // <p>In college, I had more exposure to the tech industry through research, independent study, and other internships. I worked as a Tech Consulting Intern with Future Wonder which provided me my first industry level project and experience.</p>
-// <p>Most recently, I have worked as a Software Developer Intern at IBM. And upon gradutation, I hope to continue my career as a Full Stack Developer.</p>
+// <p>Most recently, I have worked as a Software Developer Intern at IBM. And upon graduation, I hope to continue my career as a Full Stack Developer.</p>
 
 export default timeline;
